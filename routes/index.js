2cad5886353bb651b551ee399c580d4a6c54d085
Rioba Ian <riobafelix@gmail.com>
2023-08-30 10:44:12 +0300

    styling of express app

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,42 @@ var router = express.Router();
 
 const messages = [
   {
-    text: "Hi there!",
+    text: "Hi Mars!",
     user: "Amando",
-    added: new Date(),
+    added: new Date().toDateString(),
   },
   {
-    text: "Hello World",
+    text: "Hello Astronauts",
     user: "Charles",
-    added: new Date(),
+    added: new Date().toDateString(),
   },
 ];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Message board", messages });
+});
+
+router.get("/new", function (req, res) {
+  res.render("form", { title: "Enter your new message here" });
+});
+
+router.post("/new", (req, res) => {
+  console.log(req.body);
+
+  if (!req.body.text && !req.body.user) {
+    console.log("Please enter a message body and a user.");
+    res.status(400).json({ msg: "Enter at least a message and a user. " });
+    return;
+  }
+
+  messages.push({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date().toJSON(),
+  });
+  res.status(201);
+  res.redirect("/");
 });
 
 module.exports = router;
